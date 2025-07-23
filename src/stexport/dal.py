49e05874ff5fdf13a7eb69fd4e7fc06d18e58496
Dima Gerasimov <karlicoss@gmail.com>
2025-07-24 00:27:35 +0100

    chore: update all ci files; switch to hatch; add ty

--- src/stexport/dal.py
+++ src/stexport/dal.py
@@ -6,6 +6,7 @@ from datetime import datetime, timezone
 from pathlib import Path
 from typing import NamedTuple
 
+from .exporthelpers import dal_helper
 from .exporthelpers.dal_helper import Json
 from .exporthelpers.logging_helper import make_logger
 
@@ -44,7 +45,7 @@ class SiteDAL(NamedTuple):
 
     @property
     def questions(self) -> Iterable[Question]:
-        return sorted(map(Question, self.j['users/{ids}/questions']), key=lambda q: q.creation_date)
+        return sorted(map(Question, self.j['users/{ids}/questions']), key=lambda q: q.creation_date)  # ty: ignore[invalid-argument-type]
 
 
 class DAL:
@@ -72,7 +73,6 @@ def demo(dal: DAL) -> None:
 
 
 def main() -> None:
-    from .exporthelpers import dal_helper
     dal_helper.main(DAL=DAL, demo=demo)
 
 
